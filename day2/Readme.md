6a742b5951ce6275c84a6d039d1892bd65fb9771
juasasnchez <juasanchezme@unal.edu.co>
2024-10-20 10:32:53 -0500

    new file:   day2/Readme.md 	new file:   day2/main.py

---
+++ day2/Readme.md
@@ -0,0 +1,8 @@
+# Day 2. 
+## Concepts Practised
+The expression `"{:.2f}".format(...)` in Python is used to format floating-point numbers to a specific format. In this case, `"{:.2f}"` means the number will be rounded to **2 decimal places**.
+
+- `{:}`: Placeholder for the value to be formatted.
+- `.2f`: Specifies the format:
+- `f`: Indicates a floating-point number.
+- `.2`: Rounds the number to **two decimal places**.
---
+++ day2/main.py
@@ -0,0 +1,12 @@
+print("Welcome to Juan's Beer Tip Calculator!")
+
+# Get the total bill amount, tip percentage, and number of people
+bill = float(input("What is the total beer tab?\n$:"))
+tip = int(input("How much tip would you like to give?\nPercent:"))
+split = int(input("How many friends are splitting the tab?\nPeople:"))
+
+# Calculate the total amount each person should pay
+total = ("{:.2f}".format((((bill * (tip / 100)) + bill) / split)))
+
+# Display the result
+print(f"Each person should pitch in: ${total}")
