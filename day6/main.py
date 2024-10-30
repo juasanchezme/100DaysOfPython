a7d1c24d529659128fcdb9f237f375923330c460
juasanchezme <juasanchezme@unal.edu.co>
2024-10-29 19:02:30 -0500

    new file:   day6/main.py

---
+++ day6/main.py
@@ -0,0 +1,16 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():    # same as while at_goal() != True:
+
+    if right_is_clear():
+        turn_right()
+        move()
+        turn_right()
+    while wall_in_front():
+        turn_left()
+
+
+    move()
