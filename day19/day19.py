60035f66442153b515944389a691cfaa17c7e225
juasanchezme <juasanchezme@unal.edu.co>
2025-03-24 10:29:31 -0500

    modified:   day19/day19.py

--- day19/day19.py
+++ day19/day19.py
@@ -1,19 +1,53 @@
-# Event listeners
-from turtle import Turtle, Screen
+from turtle import Screen, Turtle
+import random
 
-tim = Turtle()
 screen = Screen()
+screen.setup(width=500, height=400)
 
-def move_forward():
-    tim.forward(10)
+race_on = False
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+picked_color = []
 
-screen.listen()
+def rand_color():
+    color = random.choice(colors)
+    if color not in picked_color:
+        picked_color.append(color)
+        return color
+    
+    else:
+        return rand_color()
 
-# when we use a function as an argumentof another function we dont put ()
-screen.onkey(key="space", fun=move_forward)
+
+class RacingTurtle:
+    def __init__(self, x, y):
+        self.turtle = Turtle(shape="turtle")
+        self.turtle.color(rand_color())  # color aleatorio
+        self.turtle.penup()
+        self.turtle.goto(x, y)
+
+# instancias
+turtles = []
+y = -60
+for i in range(6):
+    turtles.append(RacingTurtle(x=-230, y = y))
+    y += 30
 
 
+# movimiento
+
+if user_bet:
+    race_on = True
+
+while race_on:               
+    for t in turtles:
+        if t.turtle.xcor()> 215:
+            race_on = False
+            print(f"the winner is the {t.turtle.pencolor()} turtle")
+
+        t.turtle.forward(random.randint(1, 10))
 
 
+    
 
 screen.exitonclick()
